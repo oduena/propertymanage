f1a572282dab6e09b431d341c0b4f504d08493b7
oduena <duenas.omar@gmail.com>
2023-05-15 22:08:48 -0600

    change to allow origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,7 +3,9 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3000',
     'http://172.16.20.4:3000',
-    'https://184.71.232.214:3000'
+    'https://184.71.232.214:3000',
+    'http://192.168.2.30:3000',
+    'http://192.168.2.30'
 ];
 
 module.exports = allowedOrigins;
